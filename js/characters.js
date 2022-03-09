4194ba13e1f647d449bcc3719eb969c06d6f40ed
John Castro <johhncaastro@gmail.com>
2022-03-09 14:04:36 -0600

    add function jquerey to characters.js file alert system broke needs debugging

--- js/characters.js
+++ js/characters.js
@@ -1,4 +1,15 @@
 'use strict';
+// this page is going to have all of the javascript that is required  to make the turn based part
+//of the game. first I will be making the characters that have objects then make the page
+//functional by adding jquery to make them fight.
+
+// CHECK LIST ====================
+// 1) MAKE CHARACTERS INTO OBJECTS - DONE
+// 2) MAKE METHOD FOR DAMAGE TO CHARACTER - DONE
+// 3) MAKE AN ALERT THAT THE GAME IS OVER IF ONE OF THE CHARACTERS DIES - DONE
+// 4) WRITE JQUERY THAT WHEN EVER YOU CLICK THE CHARACTER (BOX) IT DOES DAMAGE - DONE
+// 5) FIX NUMBER 3 BECAUSE ALERT NO LONGER WORKS
+
 
 var character ={
     type: 'Mage',
@@ -14,20 +25,25 @@ var character ={
     }],
     healingPoints: 2,
     damage:function (num){
-        console. log (character.type+' took '+ num + ' Damage you now have '+ (this.healthPoints = this.healthPoints -= num)+ ' HP')
+         return (character.type+' took '+ num + ' Damage you now have '+ (this.healthPoints = this.healthPoints -= num)+ ' HP');
     },
     passiveHeal:function (){
-        console.log(this.type+ ' has healed '+ this.healingPoints + ' HP '+this.type+' now has '+(this.healthPoints = this.healthPoints += this.healingPoints)+ ' HP using Passive Heal!')
+        return (this.type+ ' has healed '+ this.healingPoints + ' HP '+this.type+' now has '+(this.healthPoints = this.healthPoints += this.healingPoints)+ ' HP using Passive Heal!');
     }
-}
-// console.log(charcter.healthPoints);
-character.damage(3);
-// console.log(charcter.healthPoints);
-character.damage(4);
-// console.log(charcter.healthPoints);
-character.passiveHeal();
-// console.log(charcter.healthPoints);
+};
 
+
+// below is console log test to show that if the damage method inside the object is used it will show up in the console. However I need a way to make it document on the screen.
+
+
+// console.log(charcter.healthPoints);
+// character.damage(3);
+// console.log(charcter.healthPoints);
+// character.damage(4);
+// console.log(charcter.healthPoints);
+// character.passiveHeal();
+// console.log(charcter.healthPoints);
+// character.damage(8)
 
 // damage:function (){
 //     this.healthPoints -= 1
@@ -35,7 +51,7 @@ character.passiveHeal();
 
 // charcter.catchPhrase('I am Mage');
 
-var charcter2 ={
+var character2 ={
     type: 'Ghost',
     healthPoints: 10,
     movementSpeed: 250,
@@ -48,10 +64,13 @@ var charcter2 ={
         evade: 3,
     }],
     damage:function (num){
-        console. log ( charcter2.type+' took '+ num + ' Damage you now have '+ (this.healthPoints = this.healthPoints -= num)+ ' HP')
+        return ( character2.type+' took '+ num + ' Damage you now have '+ (this.healthPoints = this.healthPoints -= num)+ ' HP');
     },
 
-}
+};
+
+
+
 
 var noName ={
     type: 'Macho Man',
@@ -68,7 +87,7 @@ var noName ={
         console.log(this.type+ ' has healed '+ this.healingPoints + ' HP '+this.type+' now has '+(this.healthPoints = this.healthPoints += this.healingPoints)+ ' HP using Passive Heal!')
     }
 
-}
+};
 // NoName is an experimental character that is not going to have access to the game unless you enter a cheat code.
 
 // noName.damage(4)
@@ -80,13 +99,25 @@ var noName ={
 //if the attack var is true use attack else wait
 
 
-var characters = [character,charcter2,noName]
+// the code below works when the 'mage' red square is clicked
+$('.box1').click(function (){
+    $('.dialog').html(character.damage(1))
+});
+
+$('.box2').click(function (){
+    $('.dialog2').html(character2.damage(1))
+});
+
+
+
+
+
+
+var characters = [character,character2,noName];
 
 characters.forEach(function (char) {
     if (char.healthPoints <= 0) {
         alert('GAME OVER ' + char.type + ' DIED')
     }
-})
-
-
+});
 
